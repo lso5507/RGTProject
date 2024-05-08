b91500ccfcbc7a3b65dcea9069e207330ecd57da
lso5507 <49707719+lso5507@users.noreply.github.com>
2024-05-09 08:58:06 +0900

    init

---
+++ src/main/java/com/rgtproject/RgtProjectApplication.java
@@ -0,0 +1,13 @@
+package com.rgtproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RgtProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RgtProjectApplication.class, args);
+	}
+
+}
